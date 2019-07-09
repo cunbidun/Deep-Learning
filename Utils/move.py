9c5e1235083f5424373cd012e6cb3ab1f95cd661
cunbidun <duy.tht.pham@gmail.com>
2019-07-09 14:31:55 +0700

    Add Utlis

---
+++ Utils/move.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+import sys
+import shutil
+
+FISRT_ITEM = 1
+LAST_ITEM = 10
+COPY_DIR = '../MNIST/Data/testing/'
+PASTE_DIR ='./test/'
+
+cnt = FISRT_ITEM - 1
+
+imgs = os.listdir(COPY_DIR)
+
+for img in imgs:
+    shutil.copy(COPY_DIR + img,PASTE_DIR + img)
+    cnt = cnt + 1
+    if(cnt == LAST_ITEM):
+         break
